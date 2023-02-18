c6a6dcb518ffc490757fec64584525c32d4b0179
CaioHenriqueCastro <caiohenriqueestevesdecastro@gmail.com>
2023-02-17 21:37:25 -0300

    ok

--- loja.css/loja.js
+++ loja.css/loja.js
@@ -1,4 +1,13 @@
 produtoJson.map((item, index) => {
     let produtoItem = document.querySelector('.models .produto-item').cloneNode(true)
     document.querySelector('.produto-area').append(produtoItem)
+
+    produtoItem.querySelector('.produto-item--img img').src = item.img
+    produtoItem.querySelector('.produto-item--price').innerHTML = `R$ ${item.price.toFixed(2)}` 
+    produtoItem.querySelector('.produto-item--name').innerHTML = item.name
+    produtoItem.querySelector('.produto-item--desc').innerHTML = item.description
+
+    produtoItem.querySelector('.produto-item a').addEventListener('click', () => {
+        console.log('clicou na pagina')
+    })
 })
